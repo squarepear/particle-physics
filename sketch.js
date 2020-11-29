681761ff932c6eb8d45fff927d9be8d3a76adf18
SquarePear <16364318+SquarePear@users.noreply.github.com>
2020-11-29 13:01:21 -0500

    ✨ Added ability to add particle on click

--- sketch.js
+++ sketch.js
@@ -7,25 +7,26 @@ function setup () {
   createCanvas(maxX, maxY)
 
   for (let i = 0; i < 36; i++) {
-    let x = random(0, maxX)
-    let y = random(0, maxY)
-    let mass = random(PI * 50, PI * 100)
-
-    if (random(0, 1) < 0.8 && i < 33) particles.push(new Particle(x, y, mass))
-    else if (random(0, 1) < 0.5) particles.push(new Attractor(x, y, mass))
-    else particles.push(new Repeller(x, y, mass))
+    particles.push(makeRandomParticle())
   }
 
   for (let i = 0; i < 6; i++) {
     let a = particles[Math.floor(random(0, particles.length))]
     let b = particles[Math.floor(random(0, particles.length))]
 
-    if (a === b) continue
-
-    springs.push(new Spring(a, b, random(150, 300), 2 * Math.pow(10, -5)))
+    if (a === b) i--
+    else springs.push(new Spring(a, b, random(150, 300), 2e-5))
   }
+}
 
-  noStroke()
+function makeRandomParticle (pos) {
+  let x = pos?.x || random(0, maxX)
+  let y = pos?.y || random(0, maxY)
+  let mass = random(PI * 50, PI * 100)
+
+  if (random(0, 1) < 0.8) return new Particle(x, y, mass)
+  else if (random(0, 1) < 0.5) return new Attractor(x, y, mass)
+  else return new Repeller(x, y, mass)
 }
 
 function draw () {
@@ -36,4 +37,8 @@ function draw () {
 
   for (let i = 0; i < particles.length; i++) particles[i].update().draw()
   for (let i = 0; i < springs.length; i++) springs[i].draw(true)
+}
+
+function mousePressed () {
+  particles.push(makeRandomParticle(createVector(mouseX, mouseY)))
 }
